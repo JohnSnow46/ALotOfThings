6043750314e6f3b0063d17943c8d891d68fecad9
Frank <franekuna@gmail.com>
2023-11-07 15:15:29 +0100

    podicag

---
+++ LinqLambda/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 5, 3, 6, 7, 2, 3, 1, 6, 7 };
+
+            var linqEnum = list.Where(n => n % 2 == 0).OrderBy(n => n);//zamienic order z where lepiej
+            foreach (int n in linqEnum) { Console.WriteLine(n); }
+
+            var ObiektCollection = new List<Person>
+            {
+                new Person{ Name = "Andrzej", DateOfBirth = new DateTime(2000,11,8)},
+                new Person{ Name = "Grzegorz", DateOfBirth = new DateTime(1998,03,2)},
+                new Person{ Name = "Franek", DateOfBirth = new DateTime(1967,12,1)},
+                new Person{ Name = "Janusz", DateOfBirth = new DateTime(2006,11,10)},
+            };
+
+            DateTime eighteenYearsAgo = DateTime.Now.AddYears(-18);
+
+            var AdultChecker = ObiektCollection.Where(d => d.DateOfBirth <= eighteenYearsAgo).ToList();
+
+            foreach (var x in AdultChecker) { Console.WriteLine(x.Name + " " + x.DateOfBirth); }
+
+            var ProductList = new List<Products>
+            {
+                new Products{Name="Frytki",Price=20.99M},
+                new Products{Name="Ziemniaki",Price=11.99M},
+                new Products{Name="Placki",Price=43.99M},
+                new Products{Name="Kluski",Price=51.99M}
+            };
+
+            var MaxPrice = ProductList.Max(p => p.Price);
+
+            //List<int> list = new List<int> { 5, 3, 6, 7, 2, 3, 1, 6, 7 };
+            var SumThree = list.Where(p => p % 3 == 0).Sum();
+            var AvgEven = list.Where(p => p % 2 == 0).Average();
+            Console.WriteLine(SumThree);
+            Console.WriteLine(AvgEven);
+
+            var GroupByName = ProductList.GroupBy(p => p.Name);
+            foreach (var x in GroupByName) 
+            {
+            
+            }
+
+            //Console.WriteLine(MaxPrice);
+
+            Console.Read();
+        }
+    }
+}
---
+++ PodciagCiag/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodciagCiag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "word";
+
+            podciag(word);
+            //FindAllSubstring(word);
+
+            Console.ReadLine();
+        }
+
+        public static void podciag(string word)
+        {
+            word = word.Remove(0,word.Length);
+            Console.WriteLine(word);
+        }
+
+        private static void FindAllSubstring(string str)
+        {
+            for (int i = 0; i < str.Length; ++i)
+            {
+                StringBuilder subString = new StringBuilder(str.Length - i);
+
+                for (int j = i; j < str.Length; ++j)
+                {
+                    subString.Append(str[j]);
+                    Console.Write(subString + "\n");
+                }
+
+
+            }
+        }
+    }
+}
